e29c413a06d99bfdcf48164b2c8986383759f394
Melisa Bok <melisabok@gmail.com>
2015-12-13 17:34:50 -0500

    add todos

--- src/test/java/technology/tabula/TestICDAR.java
+++ src/test/java/technology/tabula/TestICDAR.java
@@ -14,18 +14,29 @@ public class TestICDAR {
 	
 	
 	@Test
-	public void testFile1() throws IOException{
+	public void testFile001() throws IOException{
+		
+		Float height = 842f; //TODO: get page height
 		
 		Page page = UtilsForTesting.getAreaFromFirstPage("src/test/resources/technology/tabula/icdar/eu-001.pdf", 
-                842f - 683, 73f, 842f - 208, 524f);
+				height - 683, 73f, height - 208, 524f); //TODO: read bbox from eu-001-reg.xml
 		
-		System.out.println(page.getHeight());
-		
-		BasicExtractionAlgorithm bea = new BasicExtractionAlgorithm();
+		BasicExtractionAlgorithm bea = new BasicExtractionAlgorithm();//TODO: always use Basic extraction?
 		
 		List<Table> tables = bea.extract(page);
-		
 		assertFalse(tables.isEmpty());
+		assertEquals(1, tables.size());
+		
+		Table table = tables.get(0);
+		
+		System.out.println(table.getBounds());
+		
+		List<RectangularTextContainer> cells = table.getCells();
+		for (RectangularTextContainer rectangularTextContainer : cells) {
+			System.out.println(rectangularTextContainer);
+			
+			//TODO: assert rectangularTextContainer with cells defined in eu-001-str.xml
+		}
 		
         StringBuilder sb = new StringBuilder();
         (new CSVWriter()).write(sb, tables.get(0));
